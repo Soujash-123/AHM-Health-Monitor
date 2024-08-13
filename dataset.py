56c632ced4d6978616d6ff51a1cd4cb59bc3f3a2
Soujash-123 <72877997+Soujash-123@users.noreply.github.com>
2024-08-13 18:56:18 +0530

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -4,9 +4,15 @@ import os
 
 # Function to read CSV files and randomly distribute rows
 def distribute_rows(csv_file1, csv_file2, output_file):
-    # Read the CSV files into DataFrames
-    df1 = pd.read_csv(csv_file1)
-    df2 = pd.read_csv(csv_file2)
+    # Define the columns to keep
+    columns_to_keep = [
+        'temperature_one', 'temperature_two', 'vibration_x', 'vibration_y', 'vibration_z',
+        'magnetic_flux_x', 'magnetic_flux_y', 'magnetic_flux_z', 'health_status'
+    ]
+    
+    # Read the CSV files into DataFrames, selecting only the specified columns
+    df1 = pd.read_csv(csv_file1, usecols=columns_to_keep)
+    df2 = pd.read_csv(csv_file2, usecols=columns_to_keep)
     
     # Combine the rows from both DataFrames
     combined_df = pd.concat([df1, df2], ignore_index=True)
@@ -18,10 +24,8 @@ def distribute_rows(csv_file1, csv_file2, output_file):
     shuffled_df.to_csv(output_file, index=False)
 
 # Example usage
-
 csv_file1 = 'DE_BOF_15438.csv'
 csv_file2 = 'NDE_BOF_15438.csv'
 output_file = 'dataset.csv'
 
 distribute_rows(csv_file1, csv_file2, output_file)
-
